647fca515ee88340b61eaef4e485c250a8046284
Arvind K <khandelwalarvind26@gmail.com>
2024-12-06 03:28:12 +0530

    Backend working fully

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ from app.services import periodic_task
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-# import aiocron
+import aiocron
 
 # Wrapper to run async function
-# @aiocron.crontab('0 6 * * *')  # First execution at 6:00 AM, then every 24 hours
-# async def run_async_task():
-#     await periodic_task()
+@aiocron.crontab('0 0 * * *')  # Execute in every 24 hours at 00:00am
+async def run_async_task():
+    '''CronJob'''
+
+    await periodic_task()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    '''Create tables on startup'''
+    '''Create tables and run cron job on startup'''
 
     # Startup logic
     await create_tables()
@@ -21,7 +23,7 @@ async def lifespan(app: FastAPI):
     # Run the function once at startup
     await periodic_task()
     # Start the scheduled task
-    # run_async_task.start()
+    run_async_task.start()
     
     yield  # The point at which the application runs
 
